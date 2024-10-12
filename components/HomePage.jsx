361a7d431214944767227c669208c9549230a3ca
uk030621 <123797901+uk030621@users.noreply.github.com>
2024-10-12 12:07:31 +0100

    Full screen size.

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -13,22 +13,25 @@ export default function HomePage() {
   
 
   return (
-    <div className="flex justify-center items-start h-screen px-4 mt-4">
-      <div className="shadow-lg p-8 bg-zinc-300/10 flex flex-col gap-2 my-6">
+    <div className="flex justify-center items-start h-screen px-4 mt-1">
+      
+      <div className="shadow-lg p-8 bg-zinc-300/10 flex flex-col gap-2 my-6 w-full h-full">
+      <h1 className="text-2xl font-bold">Welcome Page</h1>
+      <div className="grid-cols-1">
+          <p className="self-center">Hello <span className="mr-8 ml-1/2 font-extralight">{firstName},</span></p>
+          <p className="self-center">Email: <span className="font-extralight">{session?.user?.email}</span></p>
+        </div>
         {/* Move button to a flex container for alignment */}
-        <div className="flex flex-start mb-3 w-fit"> 
+        <div className="flex flex-start mb-3 w-fit mt-5"> 
           <button
               onClick={() => signOut()}
               className="bg-red-500 text-white text-sm py-2 px-4 rounded-md"
             >
               Log Out
             </button>
-            <Link className="self-center ml-4" href="/Regform">or ➡️ Register</Link> 
+            <Link className="self-center ml-2" href="/Regform">or Main Page <span className="text-green-500">➡️</span> </Link> 
         </div>
-        <div className="grid-cols-1">
-          <p className="self-center">Hello <span className="mr-8 ml-1/2 font-extralight">{firstName},</span></p>
-          <p className="self-center">Email: <span className="font-extralight">{session?.user?.email}</span></p>
-        </div>
+        
           
         
         <div className="flex justify-center mb-6 mt-6"> 
